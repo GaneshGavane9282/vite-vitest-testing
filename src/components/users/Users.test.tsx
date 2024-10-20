373892dd21241f5d3897c5bc2d91d08a18593385
@ganesh.cg <ganesh.cg@testyantra.com>
2024-10-20 22:12:31 +0530

    add static analysis prettier

--- src/components/users/Users.test.tsx
+++ src/components/users/Users.test.tsx
@@ -1,24 +1,24 @@
-import { render, screen } from "@testing-library/react";
-import Users from "./Users";
-import { server } from "../../mocks/Server";
-import { http, HttpResponse } from "msw";
+import { render, screen } from '@testing-library/react';
+import Users from './Users';
+import { server } from '../../mocks/Server';
+import { http, HttpResponse } from 'msw';
 
-describe("User Component", () => {
-  test("render correctly", () => {
+describe('User Component', () => {
+  test('render correctly', () => {
     render(<Users />);
     const textElement = screen.getByText(/users/i);
     expect(textElement).toBeInTheDocument();
   });
 
-  test("renders a list of users", async () => {
+  test('renders a list of users', async () => {
     render(<Users />);
-    const userElements = await screen.findAllByRole("listitem");
+    const userElements = await screen.findAllByRole('listitem');
     expect(userElements).toHaveLength(3);
-  });
+  })
 
-  test("renders error", () => {
+  test('renders error', () => {
     server.use(
-      http.get("https://jsonplaceholder.typicode.com/users", () => {
+      http.get('https://jsonplaceholder.typicode.com/users', () => {
         return HttpResponse.error();
       })
     );
